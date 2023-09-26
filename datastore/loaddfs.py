5de6f6a0426e8e7e9c39e57c0ae881af2f47cc20
kubiteddie <eddiekubit0@gmail.com>
2023-09-25 20:38:34 -0400

    begin loading data, jupyter for documentation

--- datastore/loaddfs.py
+++ datastore/loaddfs.py
@@ -1,2 +1,13 @@
 import pandas as pd
+import requests
+from io import StringIO
+
+
+spreads_csv_url = "https://www.kaggle.com/datasets/tobycrabtree/nfl-scores-and-betting-data?select=spreadspoke_scores.csv"
+response = requests.get(spreads_csv_url)
+
+if response.status_code == 200:
+    spreads = pd.read_csv(StringIO(response.text), on_bad_lines='skip')
+else:
+    print(f"error reading spreads - error code {response.status_code}", )
 
